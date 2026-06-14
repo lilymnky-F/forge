df46951821eeaebe3e4749614adc37f381fa4215
Ivy French <jlljfrench@gmail.com>
2026-06-14 02:07:12 -0700

    Add randomized enemies to biomes on connect

--- forge-gui-mobile/src/forge/adventure/archipelago/APConnectEvents.java
+++ forge-gui-mobile/src/forge/adventure/archipelago/APConnectEvents.java
@@ -1,9 +1,16 @@
 package forge.adventure.archipelago;
 
+import forge.adventure.data.BiomeData;
+import forge.adventure.world.WorldSave;
 import io.github.archipelagomw.Client;
 import io.github.archipelagomw.events.ArchipelagoEventListener;
 import io.github.archipelagomw.events.ConnectionResultEvent;
 import io.github.archipelagomw.network.ConnectionResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class APConnectEvents {
@@ -25,6 +32,18 @@ public class APConnectEvents {
                 // Inform the user the apworld they connected to is invalid? IDK how this would happen but good to have ig
                 Archipelago.run(Client::close);
                 return;
+            }
+
+            List<BiomeData> biomeData = WorldSave.getCurrentSave().getWorld().getData().GetBiomes();
+            for (Map.Entry<String, ArrayList<String>> entry: slot.enemy_locations.entrySet()) {
+                String key = entry.getKey();
+                ArrayList<String> value = entry.getValue();
+
+                for (BiomeData biome: biomeData){
+                    if (Objects.equals(biome.name, key)){
+                        biome.enemies = value.toArray(new String[0]);
+                    }
+                }
             }
 
             Archipelago.run(archipelago -> {
