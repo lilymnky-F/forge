f66364bfd37a4fdc7f24dd64024200653531de10
Ivy French <jlljfrench@gmail.com>
2026-06-14 17:06:12 -0700

    Add Child Sets

--- forge-gui-mobile/src/forge/adventure/archipelago/APReceiveItemEvents.java
+++ forge-gui-mobile/src/forge/adventure/archipelago/APReceiveItemEvents.java
@@ -10,9 +10,11 @@ import io.github.archipelagomw.events.ArchipelagoEventListener;
 import io.github.archipelagomw.events.ReceiveItemEvent;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import static forge.adventure.archipelago.APData.CHILD_SETS;
 import static forge.adventure.archipelago.APPersistentState.allowedBiomes;
 import static forge.adventure.archipelago.APPersistentState.allowedFullBiomes;
 import static forge.adventure.data.RewardData.initializeAllCards;
@@ -49,16 +51,32 @@ public class APReceiveItemEvents {
             System.out.println("Added " + amount + " " + edition.getCode() + " booster(s)");
         }
         if (event.getItemName().contains("Access")) {
+            // Find the name of the set to include, should be first string in the item
             String[] s = event.getItemName().split(" ");
             CardEdition edition = StaticData.instance().getCardEdition(s[0]);
             if (edition == null)
                 System.out.println("Cannot find edition: " + s[0]);
+
+            // Load the Config, to add it to the allowed sets
             ConfigData configData = Config.instance().getConfigData();
             Set<String> allowed = new HashSet<>();
             if (configData.allowedEditions != null) {
                 allowed = new HashSet<>(Arrays.asList(configData.allowedEditions));
             }
             allowed.add(s[0]);
+
+            // Check for Child Sets
+            HashMap<String, String> poteintalChildSets = CHILD_SETS.get(s[0]);
+            if (poteintalChildSets != null){
+                for (String set_type: Archipelago.getSlotData().include_extras) {
+                    String child_set = poteintalChildSets.get(set_type);
+                    if (child_set != null) {
+                        allowed.add(child_set);
+                    }
+                }
+            }
+
+            // Add all found sets to allowed editions
             configData.allowedEditions = allowed.toArray(new String[0]);
             initializeAllCards();
             System.out.println("Added set " + s[0] + " to allowed sets.");
